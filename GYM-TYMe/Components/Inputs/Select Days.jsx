db5858691da9a5971ac3d6889b576c420f5c43d1
Charlie <106312897+civilspeedy@users.noreply.github.com>
2024-05-16 12:30:03 +0100

    object duping

--- GYM-TYMe/Components/Inputs/Select Days.jsx
+++ GYM-TYMe/Components/Inputs/Select Days.jsx
@@ -1,20 +1,21 @@
 import { useEffect, useState } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { impactAsync } from 'expo-haptics';
-import { sJson } from '../../logic/storage';
+import { sJson, setDays } from '../../logic/storage';
 
 export default function SelectDays() {
     const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
     const Day = ({ dayName }) => {
         const [colour, setColour] = useState('black');
         const [txtClr, setTxtClr] = useState('white');
-        const [borderColour, setBorderColour] = useState('white')
         const [state, setState] = useState(false);
 
         useEffect(() => {
             impactAsync();
+
             if (state) {
-                setColour('#4969B6');
+                setColour(sJson.primary);
                 setTxtClr('black');
             } else {
                 setColour('black');
@@ -22,10 +23,15 @@ export default function SelectDays() {
             }
         }, [state]);
 
+        const handlePress = () => {
+            setDays(dayName);
+            setState(!state);
+        };
+
         return (
             <Pressable
-                style={[stl.dayBtn, { borderColor: borderColour, backgroundColor: colour }]}
-                onPress={() => setState(!state)}>
+                style={[stl.dayBtn, { backgroundColor: colour }]}
+                onPress={() => handlePress()}>
                 <Text style={([stl.btnTxt], { color: txtClr })}>{dayName}</Text>
             </Pressable>
         );
@@ -60,8 +66,9 @@ const stl = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         margin: 2,
+        borderColor: 'white',
     },
     btnTxt: {
         flex: 1,
-    }
+    },
 });
--- GYM-TYMe/logic/storage.js
+++ GYM-TYMe/logic/storage.js
@@ -1,5 +1,4 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useColorScheme } from 'react-native';
 
 // fragments from https://react-native-async-storage.github.io/async-storage/docs
 
@@ -30,25 +29,78 @@ const checkLaunchedBefore = (firstLaunch) => {
 };
 
 export const setDays = async (val) => {
+    console.log(val);
     try {
-        await AsyncStorage.setItem('days', val);
+        const json = await checkDaysExists();
+        const jsonWithVal = updateDayObj(val, json);
+        await AsyncStorage.setItem('days', jts(jsonWithVal));
+        console.log(jsonWithVal); // object duping
     } catch (e) {
         console.error('Err in setDays ', e);
     }
-}
+};
+
+const resetDays = async () => {
+    try {
+        await AsyncStorage.removeItem('days');
+    } catch (e) {
+        console.error('err in resetDays ', e);
+    }
+};
 
 export const getDays = async () => {
     try {
         const val = await AsyncStorage.getItem('days');
-        return val;
+        return stj(val);
     } catch (e) {
-        console.error('Err in getDays ',e )
+        console.error('Err in getDays ', e);
     }
-}
+};
 
+const checkDaysExists = async () => {
+    try {
+        const days = await getDays();
+        console.log('days: ', days);
+        if (days === null || days === undefined) {
+            return {
+                mon: false,
+                tue: false,
+                wed: false,
+                thu: false,
+                fri: false,
+                sat: false,
+                sun: false,
+            };
+        } else {
+            return days;
+        }
+    } catch (e) {
+        console.error('err in checkDaysExists ', e);
+    }
+};
+
+const updateDayObj = (dayString, daysObject) => {
+    switch (dayString) {
+        case 'Mon':
+            daysObject.mon = true;
+        case 'Tue':
+            daysObject.tue = true;
+        case 'Wed':
+            daysObject.wed = true;
+        case 'Thu':
+            daysObject.thu = true;
+        case 'Fri':
+            daysObject.fri = true;
+        case 'Sat':
+            daysObject.sat = true;
+        case 'Sun':
+            daysObject.sun = true;
+    }
+    return daysObject;
+};
 
 const jts = (val) => JSON.stringify(val);
 
-const stj = (value) => (value ? JSON.parse(val) : null);
+const stj = (val) => (val ? JSON.parse(val) : null);
 
-export const sJson = require('../data/style.json')
+export const sJson = require('../data/style.json');
