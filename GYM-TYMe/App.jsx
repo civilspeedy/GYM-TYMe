f1479a32b14f52a8aeb05b2f256f6ab333f3bef8
Charlie <106312897+civilspeedy@users.noreply.github.com>
2024-05-26 17:04:56 +0100

    blank slate

--- GYM-TYMe/App.jsx
+++ GYM-TYMe/App.jsx
@@ -1,24 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, useColorScheme } from 'react-native';
-import { useEffect, useState } from 'react';
-import { getLaunchedBefore } from './logic/storage';
-import LaunchPage from './Components/LaunchPage';
-import ProgressPage from './Components/ProgressPage';
 
 export default function App() {
-  const [launchedBefore, setLaunchedBefore] = useState(false);
-  const systemTheme = useColorScheme(); // not working correctly
-
-  getLaunchedBefore()
-    .then((val) => {
-      setLaunchedBefore(val);
-    })
-    .catch((e) => console.error('Err in firstLaunch fetch ', e));
-
   return (
     <View style={styles.container}>
       <StatusBar style='auto' />
-      <LaunchPage />
     </View>
   );
 }
