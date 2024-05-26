86d40a6fdcdaf7fd177b39dfa152f60b4f6e4266
Charlie <106312897+civilspeedy@users.noreply.github.com>
2024-05-26 21:33:26 +0100

    getting and updating

--- GYM-TYMe/logic/storage.js
+++ GYM-TYMe/logic/storage.js
@@ -4,6 +4,57 @@
  */
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+const split = {
+  name: null,
+  dayOfWeek: null,
+  exerciseList: [],
+};
+
+const exercise = {
+  id: null,
+  exercise: null,
+  done: false,
+};
+
+const weightExercise = {
+  name: null,
+  weight: null,
+  reps: null,
+  sets: null,
+};
+
+const time = {
+  hours: 0,
+  minutes: 0,
+  seconds: 0,
+  milliseconds: 0,
+};
+
+const cardio = {
+  name: null,
+  time: time,
+};
+
+const cardioDistance = {
+  name: null,
+  time: time,
+  distance: null,
+};
+
+/**
+ *Parses a json(object) to a string
+ * @param {object} val the object value to parsed as a string
+ * @returns {string} the object as a string
+ */
+const jts = (val) => JSON.stringify(val);
+
+/**
+ * Parses a string into a json (object)
+ * @param {string} val
+ * @returns {object} the string now as an object
+ */
+const stj = (val) => (val ? JSON.parse(val) : null);
+
 /**
  * A simple function for storing any data
  * @param {string} key the key to identify the data with
@@ -69,16 +120,143 @@ export async function checkIfExists(item) {
     return false;
   }
 }
-/**
- *Parses a json(object) to a string
- * @param {object} val the object value to parsed as a string
- * @returns {string} the object as a string
- */
-const jts = (val) => JSON.stringify(val);
 
 /**
- * Parses a string into a json (object)
- * @param {string} val
- * @returns {object} the string now as an object
+ * function to store a new split, first the storage location is checked to see if it already exits
+ * @param {string} name the name the user wishes to give to the spit
+ * @param {string} dayOfTheWeek the day of the week the split will be assigned to
  */
-const stj = (val) => (val ? JSON.parse(val) : null);
+async function storeSplit(name, dayOfWeek) {
+  const item = split;
+  item.name = name;
+  item.dayOfWeek = dayOfWeek;
+
+  let list = [];
+  if (checkIfExists('splits')) {
+    list = await getItem('splits');
+    if (!Array.isArray(list)) {
+      console.error('Error in handleSplitStorage: not Array');
+    }
+  }
+  list.push(item);
+  storeItem('splits', item);
+}
+
+/**
+ * Returns all splits in storage
+ * @returns {Array} an array containing all split objects
+ */
+async function getSplits() {
+  const item = await getItem('splits');
+  if (checkIfExists(item)) {
+    return item;
+  } else {
+    console.error(
+      'Error in getSplits: You are fetching for splits where there are none.'
+    );
+    return null;
+  }
+}
+
+/**
+ * Updates data in a stored split, excluding exercise data
+ * @param {string} splitName the name of the split to be updated
+ * @param {string} dataType the type of data to be updated
+ * @param {*} replacementData the new data
+ */
+async function updateSplitData(splitName, dataType, replacementData) {
+  const splits = getSplits();
+  let selectedSplit = null;
+
+  let counter = 0;
+  for (const split in splits) {
+    counter++;
+    if (split.name == splitName) {
+      selectedSplit = split;
+      break;
+    }
+  }
+
+  if (selectedSplit === null) {
+    console.error('Error in updateSplits: there are no splits with this name');
+  }
+
+  switch (dataType) {
+    case 'name':
+      selectedSplit.name = replacementData;
+    case 'dayOfWeek':
+      selectedSplit.dayOfWeek = replacementData;
+  }
+
+  splits[counter] = selectedSplit;
+
+  storeItem('splits');
+}
+
+// add exercise to split
+
+async function updateExercise(
+  splitName,
+  exerciseId,
+  exerciseType,
+  dataType,
+  replacementData
+) {
+  const splits = getSplits();
+
+  let selectedExercise = null;
+  let splitCount = 0;
+  let exerciseCount = 0;
+
+  for (const split in splits) {
+    splitCount++;
+    if (split.name == splitName) {
+      for (const exerciseInList in split.exerciseList) {
+        exerciseCount++;
+        if (exercise.id == exerciseId) {
+          selectedExercise = exerciseInList.exercise;
+          break;
+        }
+      }
+    }
+  }
+
+  if (selectedExercise === null) {
+    console.log('Error in updateExercise: could not find exercise');
+    return false;
+  }
+
+  if (dataType == 'name') {
+    selectedExercise.name = replacementData;
+    return true;
+  }
+
+  if (exerciseType == 'weight') {
+    switch (dataType) {
+      case 'weight':
+        selectedExercise.weight = replacementData;
+      case 'reps':
+        selectedExercise.reps = replacementData;
+      case 'sets':
+        selectedExercise.sets = replacementData;
+    }
+    return true;
+  }
+
+  if (
+    (exerciseType == 'cardio' || exerciseType == 'distanceCardio') &&
+    dataType == 'time'
+  ) {
+    selectedExercise.time = replacementData;
+    return true;
+  } else {
+    if (exerciseType == 'distanceCardio' && dataType == 'distance') {
+      selectedExercise.distance = replacementData;
+      return true;
+    }
+    console.error(
+      'Error in updateExercise: could not find exercise or dataType'
+    );
+    return false;
+  }
+}
